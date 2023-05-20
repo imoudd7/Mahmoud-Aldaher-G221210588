e118fd5a0844c569c070c480c332d17333385c2c
Mahmoud Daher <132212620+imoudd7@users.noreply.github.com>
2023-05-20 11:39:21 +0300

    commit
    commit php after edit

---
+++ contact.php
@@ -0,0 +1,17 @@
+
+<?php
+
+echo "<h3>form bilgileri: </h3>" ;
+echo "<b>Adı:</b>". $_POST["ad"] ;
+echo "<br>";
+echo "<b>Soyadı:</b>". $_POST["Soyad"] ;
+echo "<br>";
+echo "<b>Eposta:</b>". $_POST["email"] ;
+echo "<br>";
+echo "<b>Cep telefonu:</b>". $_POST["cep"] ;
+echo "<br>";
+echo "<b>cinsiyet:</b>". $_POST["cinsiyet"] ;
+echo "<br>";
+echo "<b>Mesaj:</b>". $_POST["mesaj"] ;
+ 
+?>
